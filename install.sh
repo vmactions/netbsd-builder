c945871434f49b4048f60878046e4e958a000e49
neil <github@neilpang.com>
2022-07-09 14:51:18 +0800

    s

--- install.sh
+++ install.sh
@@ -102,7 +102,7 @@ $vmsh startVM $osname
 
 
 
-waitForText "NetBSD/amd64 (Amnesiac) (constty)"
+waitForText "NetBSD/amd64 (localhost) (constty)"
 
 sleep 2
 
@@ -110,5 +110,34 @@ inputKeys "string root; enter"
 
 sleep 2
 
-$vmsh screenText $osname
+
+echo '
+
+sed -i "s/PermitRootLogin no/PermitRootLogin yes/" /etc/ssh/sshd_config
+
+sed -i "s/#PermitEmptyPasswords no/PermitEmptyPasswords yes/" /etc/ssh/sshd_config
+
+sed -i "s/#PubkeyAuthentication no/PubkeyAuthentication yes/" /etc/ssh/sshd_config
+
+
+echo "AcceptEnv   *"  >> /etc/ssh/sshd_config
+
+mkdir -p ~/.ssh
+
+chmod -R 600 ~/.ssh
+
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
+
+echo "StrictHostKeyChecking=accept-new" >>~/.ssh/config
+
+
+service  sshd restart
+
+
+
+
+' >enablessh.txt
+
+
+$vmsh inputFile $osname enablessh.txt
 
