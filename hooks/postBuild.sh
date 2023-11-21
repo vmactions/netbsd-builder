62b3beb328924e9ec7bc1f278391306d1ab5a9c7
neil <neilgit@neilpang.com>
2023-11-21 21:27:07 +0100

    fix ca certs

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -1,10 +1,11 @@
-#some tasks run in the VM as soon as the vm is up
+#some tasks run in the VM as soon as the vm is built and up
 
 
 
 /usr/sbin/pkg_add  mozilla-rootcerts
 
 rm -f /usr/pkg/etc/openssl/certs/*
+rm -rf /etc/openssl/certs/*
 
 /usr/pkg/sbin/mozilla-rootcerts  install
 
