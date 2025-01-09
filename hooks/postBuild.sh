b173bcbc031010075ef74454c9f3697745c67c65
neil <gitpc@neilpang.com>
2025-01-09 19:41:29 +0100

    add aarch64

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -1,6 +1,11 @@
 #some tasks run in the VM as soon as the vm is built and up
 
 
+if uname -a | grep "evbarm"; then
+  export PKG_PATH="http://cdn.netbsd.org/pub/pkgsrc/packages/NetBSD/aarch64/$VM_RELEASE/All/"
+  echo "export PKG_PATH=\"http://cdn.netbsd.org/pub/pkgsrc/packages/NetBSD/aarch64/$VM_RELEASE/All/\"" >>/etc/profile
+fi
+
 
 /usr/sbin/pkg_add  mozilla-rootcerts
 
